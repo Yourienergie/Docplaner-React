8bda963ecd3f754d5dab4c781619e11188fbf59c
belgacem negrini <belgacemnegrini@gmail.com>
2020-06-19 10:23:47 +0200

    first commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,16 +6,17 @@ const Navbar = (props) => {
             <img className="logo-docplanner" src="https://www.docplanner.com/img/logo-default-group-en.svg?v=1" />
 
             <ul className='namenav'>
-                {props.data.map(el => el.subMenu ? <div class="dropdown">
-                    <a class="dropbtn">{el.text}</a>
-                    <div class="dropdown-content">
-                    {
-                        el.subMenu.map(el =><a href="#">{el}</a> )
-                    }
-                        
-                       
+                {props.data.map(el => el.subMenu ?
+                    <div class="dropdown">
+                        <a class="dropbtn">{el.text}</a>
+                        <div class="dropdown-content">
+                            {
+                                el.subMenu.map(el => <a href="#">{el}</a>)
+                            }
+
+                        </div>
                     </div>
-                </div> : <a href='#'><li className='nav'>{el.text}</li></a>)}
+                    : <a href='#'><li className='nav'>{el.text}</li></a>)}
             </ul>
 
 
